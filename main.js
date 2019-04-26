1005f6dbf142139e675479f7efe266453099050f
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-04-26 17:19:02 +0200

    added 0s to h,m,s

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const clock = document.querySelector(".clock");
 const tick = () => {
   const now = new Date();
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const s = now.getSeconds();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+
+  if (s < 10) {
+    s = "0" + s;
+  } else if (m < 10) {
+    m = "0" + m;
+  } else if (h < 10) {
+    h = "0" + h;
+  }
+
   const html = `
   <span>${h}</span> :
   <span>${m}</span> :
-  <span>${s}</span>
+  <span class="secondss">${s}</span>
   `;
+
   clock.innerHTML = html;
 };
 setInterval(tick, 1000);
